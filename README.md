9d13432bdc818481f770efef9f29e024efda9392
zimice <simon.kochanek@me.com>
2021-11-28 11:42:02 +0100

    first working version

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-https://techtutorialsx.com/2018/10/12/esp32-http-web-server-handling-body-data/
+# ESP32 display HTTP POST 
+
+## [Main resource](https://techtutorialsx.com/2018/10/12/esp32-http-web-server-handling-body-data/)
+
+### This project is dedicated to using esp32 as web server and handling the HTTP POST requests.It should display the message in the raw body of post.
+<br />
+
+## Libraries needed to add to project in Arduino ide:
+
+- [ESPAsyncWebServer](https://github.com/me-no-dev/ESPAsyncWebServer)
+- [AsyncTCP - for ESP32](https://github.com/me-no-dev/AsyncTCP)
+- [ESPAsyncTCP - for ESP8266](https://github.com/me-no-dev/ESPAsyncTCP)
+- ArduinoJson 6.18 in Arduino IDE
+I used OLED 0.96 128x64 and libreries were:
+- Arduino SSD1306
+- Arduino GFX
+  
+### I used ESP32 with included pinout
+
+![](img/ESP32-Pinout.jpg)
+
+<br />
+
+### Settings of Tools:
+
+![](img/tools_options.png)
+
+I connected display to 3.3V and GND with SCL and SDA 
--- esp-displaypost.ino
+++ esp-displaypost.ino
@@ -1,16 +1,89 @@
-#include "http_server.h"
+s #include <ArduinoJson.h>
 
-void setup(){
-     Serial.begin(9600);  // setup serial communication
-     // Set WiFi to station(STA) and access point (AP) mode simultaneously 
-     WiFi.mode(WIFI_AP_STA);
-     delay(100); // Recommended delay
-     WiFi.softAP("choose_some_ssid", "choose_some_password");
+#include <AsyncTCP.h>
 
-     // Remember to configure the previous server
-     setup_http_server();  // starts the asyncronus https server*/
+#include <AsyncEventSource.h>
+#include <AsyncJson.h>
+#include <AsyncWebSocket.h>
+#include <AsyncWebSynchronization.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFSEditor.h>
+#include <StringArray.h>
+#include <WebAuthentication.h>
+#include <WebHandlerImpl.h>
+#include <WebResponseImpl.h>
+
+#include <ETH.h>
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiClient.h>
+#include <WiFiGeneric.h>
+#include <WiFiMulti.h>
+#include <WiFiScan.h>
+#include <WiFiServer.h>
+#include <WiFiSTA.h>
+#include <WiFiType.h>
+#include <WiFiUdp.h>
+
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+ 
+const char* ssid = "";
+const char* password =  "";
+ 
+AsyncWebServer server(80);
+ 
+void setup() {
+  Serial.begin(115200);
+
+if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println(WiFi.localIP());
+ 
+  server.on(
+    "/post",
+    HTTP_POST,
+    [](AsyncWebServerRequest * request){},
+    NULL,
+    [](AsyncWebServerRequest * request, uint8_t *data, size_t len, size_t index, size_t total) {
+      
+ display.clearDisplay();
+
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 15);
+  // Display static text
+  char tex[len];
+ for (size_t i = 0; i < len; i++) {
+        Serial.write(data[i]);
+        tex[i] = data[i];
+      }
+       Serial.println();
+       Serial.println(tex);
+       display.println(tex);
+  display.display(); 
+     
+ 
+      request->send(200);
+  });
+ 
+  server.begin();
 }
-
-void loop(){
-     delay(1000);
-}
+ 
+void loop() {}
